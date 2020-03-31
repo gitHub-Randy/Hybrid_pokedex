ebff95c39524a465bcf65f9b3a57acf24e70725b
Randy <randy.anft@gmail.com>
2020-03-31 13:43:15 +0200

    fasdff

--- HybridDex/src/app/pages/tab2/tab2.page.ts
+++ HybridDex/src/app/pages/tab2/tab2.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Map, latLng, tileLayer, Layer, marker,Marker, icon, layerGroup } from 'leaflet';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { Platform } from '@ionic/angular';
+import {PokemonService} from "../../services/pokemon.service";
 
 
 @Component({
@@ -11,13 +13,24 @@ import { Geolocation } from '@ionic-native/geolocation/ngx';
 export class Tab2Page implements OnInit {
   map: Map;
   locationMarker: marker;
-  constructor(private geolocation: Geolocation) {
-    this.getLocation();
+  spawnedPokemon = [];
+  ownLoc = [];
+  constructor(private geolocation: Geolocation, private platform: Platform, private pokemonService: PokemonService) {
+    this.platform.ready().then(() => {
+      this.getLocation();
+
+    });
+    this.spawnPokemons();
+
   }
 
   ngOnInit() {
   }
 
+  ionViewDidLoad(){
+    console.log("yeet")
+
+    }
 
   ionViewDidEnter() { this.leafletMap(); }
 
@@ -70,13 +83,15 @@ export class Tab2Page implements OnInit {
     this.geolocation.getCurrentPosition().then((resp) => {
       // resp.coords.latitude
       // resp.coords.longitude
+      console.log(resp)
+      this.ownLoc.push(resp.coords.latitude, resp.coords.longitude);
     }).catch((error) => {
       console.log('Error getting location', error);
     });
 
     let watch = this.geolocation.watchPosition();
     watch.subscribe((data) => {
-      console.log(data.coords);
+
       this.removeMarker();
       this.addMarker(data.coords.latitude,data.coords.longitude);
       // data can be a set of coordinates, or an error (if an error occurred).
@@ -84,4 +99,56 @@ export class Tab2Page implements OnInit {
       // data.coords.longitude
     });
   }
+
+
+
+  spawnPokemons(){
+    for(let i = 0; i< 10; i++){
+      // @ts-ignore
+      let randomPokemonIndex = Math.floor((Math.random() * 125) + 1);
+      this.pokemonService.getPokeDetails(randomPokemonIndex).subscribe(res =>{
+        let pokemon = res;
+        console.log(" YEEEEET")
+        console.log(this.ownLoc)
+        let coords = this.generateNearbyLocation(parseInt(this.ownLoc[0]), parseInt(this.ownLoc[1]), 10)
+        let newPokemon = {
+          'Latitude': coords[0], 'Longitude': coords[1], 'Pokemon': pokemon.name,
+          'ImgURL': pokemon.images[2], 'Id': pokemon.id
+        };
+        this.spawnedPokemon.push(newPokemon);
+        this.addPokeMarkers(pokemon.images[2])
+
+
+      })
+    }
+    console.log(this.spawnedPokemon)
+  }
+
+
+  addPokeMarkers(iconUrl){
+    const iconDefault = icon({
+      iconUrl,
+      iconSize: [35, 35], // size of the icon
+
+      iconAnchor: [12,41],
+      tooltipAnchor:[16,-28]
+    })
+    Marker.prototype.options.icon = iconDefault;
+    console.log(this.spawnedPokemon)
+    for(let i = 0; i< this.spawnedPokemon.length;i ++){
+      let pokeMarker = marker([this.spawnedPokemon[i].Latitude, this.spawnedPokemon[i].Longitude], {
+        draggable:
+            false
+      });
+      pokeMarker.addTo(this.map);
+    }
+  }
+
+  generateNearbyLocation(lat,lng){
+    var randomLat = Math.random() * (lat - 51.6850) + 51.6850
+    var randomLon = Math.random() * (5.3200 - 5.2800) + 5.2800
+    return [randomLat, randomLon];
+  }
+
+
 }
