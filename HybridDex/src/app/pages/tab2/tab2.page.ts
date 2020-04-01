b2b994a8e20b83099dcd872235d583ae56c1fb7e
Randy <randy.anft@gmail.com>
2020-04-01 14:54:52 +0200

    yeet

--- HybridDex/src/app/pages/tab2/tab2.page.ts
+++ HybridDex/src/app/pages/tab2/tab2.page.ts
@@ -8,9 +8,9 @@ import {map} from "rxjs/operators";
 
 
 @Component({
-    selector: 'app-tab2',
-    templateUrl: './tab2.page.html',
-    styleUrls: ['./tab2.page.scss'],
+  selector: 'app-tab2',
+  templateUrl: './tab2.page.html',
+  styleUrls: ['./tab2.page.scss'],
 })
 export class Tab2Page implements OnInit {
   map: Map;
@@ -42,8 +42,6 @@ export class Tab2Page implements OnInit {
   ionViewDidLoad(){
     console.log("yeet")
 
-        });
-        this.spawnPokemons();
     }
 
   ionViewDidEnter() {
@@ -64,52 +62,45 @@ export class Tab2Page implements OnInit {
     })
     }
 
-    ionViewDidLoad() {
-        console.log('yeet');
+  leafletMap() {
+    // In setView add latLng and zoom
+    this.map = new Map('map').setView([51.877229,5.523009], 17);
+    tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
 
+  }
+
+
+
+  setLocationMarker(){
+    var iconUrl = '../../assets/trainer.png';
+    var shadowUrl = '../../assets/marker-shadow.png';
+    const iconDefault = icon({
+      iconUrl,
+      iconSize:     [35, 35], // size of the icon
+
+      iconAnchor: [12,41],
+      tooltipAnchor:[16,-28]
+    })
+    Marker.prototype.options.icon = iconDefault;
+  }
+
+    addMarker(latitude,longitude){
+    this.setLocationMarker();
+      this.locationMarker = marker([latitude, longitude], {
+        draggable:
+            false
+      });
+      this.locationMarker.addTo(this.map);
     }
 
-    ionViewDidEnter() {
-        this.leafletMap();
+    removeMarker(){
+    if(this.locationMarker != null)
+    this.map.removeLayer(this.locationMarker);
     }
 
-    leafletMap() {
-        // In setView add latLng and zoom
-        this.map = new Map('map').setView([51.877229, 5.523009], 17);
-        tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution:
-                '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(this.map);
-
-    }
-
-    setLocationMarker() {
-        var iconUrl = '../../assets/trainer.png';
-        var shadowUrl = '../../assets/marker-shadow.png';
-        const iconDefault = icon({
-            iconUrl,
-            iconSize: [35, 35], // size of the icon
-
-            iconAnchor: [12, 41],
-            tooltipAnchor: [16, -28]
-        });
-        Marker.prototype.options.icon = iconDefault;
-    }
-
-    addMarker(latitude, longitude) {
-        this.setLocationMarker();
-        this.locationMarker = marker([latitude, longitude], {
-            draggable:
-                false
-        });
-        this.locationMarker.addTo(this.map);
-    }
-
-    removeMarker() {
-        if (this.locationMarker != null) {
-            this.map.removeLayer(this.locationMarker);
-        }
-    }
 
   /** Remove map when we have multiple map object */
   ionViewWillLeave() {
@@ -157,16 +148,19 @@ export class Tab2Page implements OnInit {
           'ImgURL': pokemon.images[3], 'Id': pokemon.id
         };
 
-    getLocation() {
-        this.geolocation.getCurrentPosition().then((resp) => {
-            this.ownLoc.push(resp.coords.latitude, resp.coords.longitude);
-        }).catch((error) => {
-            console.log('Error getting location', error);
-        });
-        let watch = this.geolocation.watchPosition();
-        watch.subscribe((data) => {
-            this.removeMarker();
-            this.addMarker(data.coords.latitude, data.coords.longitude);
+        this.spawnedPokemon.push(newPokemon);
+        const iconDefault = icon({
+          iconUrl: newPokemon.ImgURL,
+          iconSize: [50, 50], // size of the icon
+          iconAnchor: [12,41],
+          tooltipAnchor:[16,-28]
+        })
+        Marker.prototype.options.icon = iconDefault;
+        Marker.prototype.options.icon = iconDefault;
+        console.log(i, "INDEX")
+        let pokeMarker = marker([newPokemon.Latitude, newPokemon.Longitude], {
+          draggable:
+              false
         });
         pokeMarker.addTo(this.map);
       })
@@ -175,35 +169,6 @@ export class Tab2Page implements OnInit {
 
     }
 
-    spawnPokemons() {
-        for (let i = 0; i < 10; i++) {
-            // @ts-ignore
-            let randomPokemonIndex = Math.floor((Math.random() * 125) + 1);
-            this.pokemonService.getPokeDetails(randomPokemonIndex).subscribe(res => {
-                let pokemon = res;
-                let coords = this.generateNearbyLocation(parseFloat(this.ownLoc[0]), parseFloat(this.ownLoc[1]));
-                let newPokemon = {
-                    'Latitude': coords[0], 'Longitude': coords[1], 'Pokemon': pokemon.name,
-                    'ImgURL': pokemon.images[3], 'Id': pokemon.id
-                };
-
-                this.spawnedPokemon.push(newPokemon);
-                const iconDefault = icon({
-                    iconUrl: newPokemon.ImgURL,
-                    iconSize: [50, 50], // size of the icon
-                    iconAnchor: [12, 41],
-                    tooltipAnchor: [16, -28]
-                });
-                Marker.prototype.options.icon = iconDefault;
-                Marker.prototype.options.icon = iconDefault;
-                console.log(i, 'INDEX');
-                let pokeMarker = marker([newPokemon.Latitude, newPokemon.Longitude], {
-                    draggable:
-                        false
-                });
-                pokeMarker.addTo(this.map);
-            });
-        }
   }
 
 
@@ -227,30 +192,8 @@ export class Tab2Page implements OnInit {
 
     return [parseFloat(newlat).toFixed(7),parseFloat(newlon).toFixed(7)]
 
-
   }
 
-    async presentAlertConfirm() {
-        const alert = await this.alertController.create({
-            header: 'Catch Pokemon',
-            message: 'Do you want to catch this pokemon?',
-            buttons: [
-                {
-                    text: 'Cancel',
-                    role: 'cancel',
-                    cssClass: 'secondary',
-                    handler: (blah) => {
-                        console.log('Confirm Cancel: blah');
-                    }
-                }, {
-                    text: 'Catch',
-                    handler: () => {
-                        console.log('Confirm Okay');
-                    }
-                }
-            ]
-        });
 
-        await alert.present();
-    }
+
 }
